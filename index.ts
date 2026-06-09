416216809f67a4791d32e783f29ebd467199f4a2
Aaron Menadue <aaron.menadue@gmail.com>
2026-06-09 09:58:07 +0930

    Edit 🚧: changes entrypoint

---
+++ index.ts
@@ -0,0 +1,20 @@
+import TwitchApp, { type ITwitchApp, type ITwitchChat } from "./src/app";
+import { type ITwitchUser } from "./src/user";
+import { type ITwitchStreamerUser } from "./src/streamer";
+import { type IEventSub } from "./src/eventsub";
+
+import { type ITwitchChatMessage, type ITwitchBadge, type TwitchChatMessageType, type TwitchChatMessageFragment, type ITwitchChatter } from "./src/chatMessage";
+
+export {
+	TwitchApp,
+	type ITwitchApp,
+	type ITwitchChat,
+	type ITwitchUser,
+	type ITwitchStreamerUser,
+	type IEventSub,
+	type ITwitchChatMessage,
+	type ITwitchBadge,
+	type TwitchChatMessageType,
+	type TwitchChatMessageFragment,
+	type ITwitchChatter,
+};
